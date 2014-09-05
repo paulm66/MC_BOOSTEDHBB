89ce50fae0d7e85446743d43a4c1644a5100170e
Chris <cspollard@gmail.com>
2014-09-05 12:20:31 +0100

    initial commit.

---
+++ MC_BOOSTEDHBB.cc
@@ -0,0 +1,78 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/FinalState.hh"
+/// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
+
+namespace Rivet {
+
+
+  class MC_BOOSTEDHBB : public Analysis {
+  public:
+
+    /// Constructor
+    MC_BOOSTEDHBB()
+      : Analysis("MC_BOOSTEDHBB")
+    {    }
+
+
+  public:
+
+    /// @name Analysis methods
+    //@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+
+      /// @todo Initialise and register projections here
+
+      /// @todo Book histograms here, e.g.:
+      // _h_XXXX = bookProfile1D(1, 1, 1);
+      // _h_YYYY = bookHisto1D(2, 1, 1);
+
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+      const double weight = event.weight();
+
+      /// @todo Do the event by event analysis here
+
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+
+      /// @todo Normalise, scale and otherwise manipulate histograms here
+
+      // scale(_h_YYYY, crossSection()/sumOfWeights()); // norm to cross section
+      // normalize(_h_YYYY); // normalize to unity
+
+    }
+
+    //@}
+
+
+  private:
+
+    // Data members like post-cuts event weight counters go here
+
+
+  private:
+
+    /// @name Histograms
+    //@{
+    Profile1DPtr _h_XXXX;
+    Histo1DPtr _h_YYYY;
+    //@}
+
+
+  };
+
+
+
+  // The hook for the plugin system
+  DECLARE_RIVET_PLUGIN(MC_BOOSTEDHBB);
+
+}
