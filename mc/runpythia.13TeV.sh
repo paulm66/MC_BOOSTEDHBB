6578900e1a0d1c11eed23cb3b2ef6c699b2224e8
Chris <cspollard@gmail.com>
2014-09-09 18:29:29 +0100

    adding output folder to runpythia.sh.

--- mc/runpythia.13TeV.sh
+++ mc/runpythia.13TeV.sh
@@ -1,1 +1,5 @@
-run-pythia -n $1 -i $2 -o ${2/cmnd/}13TeV.hepmc --collision-energy 13000 >& ${2/cmnd/}13TeV.log
+# $1 -> number of events
+# $2 -> input cmnd file
+# $3 -> output folder
+
+run-pythia -n $1 -i $2 -o $3/${2/cmnd/}13TeV.hepmc --collision-energy 13000 >& $3/${2/cmnd/}13TeV.log
