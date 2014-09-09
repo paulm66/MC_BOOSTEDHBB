9c91fdd7aea60c3d52c0de98d2d9c30cf2a3cbef
Chris <cspollard@gmail.com>
2014-09-09 12:07:55 +0100

    changing test script to run on pre-made events.

--- test.sh
+++ test.sh
@@ -1,11 +1,1 @@
-run-pythia \
-    -n $1 \
-    -o hepmc.fifo \
-    --collision-energy 13000 \
-    -c "HiggsSM:ffbar2HZ=on" \
-    -c "HiggsSM:ffbar2HW=on" \
-    -c "25:onMode=off" \
-    -c "25:onIfMatch=5 -5" &
-
-rivet -a MC_BOOSTEDHBB -H MC_BOOSTEDHBB.yoda \
-    --pwd hepmc.fifo
+rivet -a MC_BOOSTEDHBB -H MC_BOOSTEDHBB.yoda --pwd $1
