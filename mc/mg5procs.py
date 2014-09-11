1c066fefbd7b964123ec0b400edd077291554bd0
Chris <cspollard@gmail.com>
2014-09-11 14:50:35 +0100

    almost done moving to python automation.

---
+++ mc/mg5procs.py
@@ -0,0 +1,21 @@
+from mg5common import mg5proc, smheader
+
+procdict = {
+"ttbarnoallhad":
+"generate p p > t t~, t > l+ v b; add process p p > t t~, t~ > l- v~ b~",
+
+"Wbb":
+"generate p p > w+ b b~, w+ > l+ v $ h; add process p p > w- b b~, w- > l- v~ $ h",
+
+"Zbb":
+"generate p p > w+ b b~, w+ > l+ v $ h; add process p p > w- b b~, w- > l- v~ $ h",
+
+"WH":
+"generate p p > w+ h, w+ > l+ v, h > b b~; add process p p > w- h, w- > l- v~, h > b b~",
+
+"ZH":
+"generate p p > z h, z > l l, h > b b~"
+}
+
+for name, cmd in procdict.iteritems():
+    procdict[name] = mg5proc(name, smheader + cmd)
