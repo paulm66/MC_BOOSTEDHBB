285be86b0778c8fa502f8d198a0e9a1b89ae4582
Chris <cspollard@gmail.com>
2014-09-15 18:00:27 +0100

    fixing up runpythia for smaller file chunks.

--- mc/runpythia.sh
+++ mc/runpythia.sh
@@ -8,9 +8,16 @@ do
     echo "perl -p -i -e "s/\"3.0\"/\"1.0\"/" $f/$f.lhe"
     perl -p -i -e "s/\"3.0\"/\"1.0\"/" $f/$f.lhe
 
-    # 999999999 -> shower all events.
-    echo "run-pythia -n 999999999 -l $f/$f.lhe -o $f/$f.hepmc >& $f/pythia.log &"
-    run-pythia -n 999999999 -l $f/$f.lhe -o $f/$f.hepmc >& $f/pythia.log &
+    python splitLHE.py -i $f/$f.lhe -n 25000
+
+    for f1 in `ls $f/${f}_*.lhe`
+    do
+
+        # 999999999 -> shower all events.
+        echo "run-pythia -n 999999999 -l $f1 -o ${f1/lhe/hepmc} >& ${f1/lhe/pythia.log} &"
+        run-pythia -n 999999999 -l $f1 -o ${f1/lhe/hepmc} >& ${f1/lhe/pythia.log} &
+
+    done
 
 done
 
