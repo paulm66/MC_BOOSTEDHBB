8fcbde1478a530b43a71f9d0c754384af4d89f44
Chris <cspollard@gmail.com>
2014-09-18 15:43:56 +0100

    I think this is where I want things to be.

--- runrivet.sh
+++
@@ -1,9 +0,0 @@
-for f in $@
-do
-    echo "rivet --pwd -a MC_BOOSTEDHBB -H ${f/hepmc/yoda} $f >& ${f/hepmc/rivet.log} &"
-    rivet --verbose --pwd -a MC_BOOSTEDHBB -H ${f/hepmc/yoda} $f >& ${f/hepmc/rivet.log} &
-done
-
-echo "all jobs submitted in runrivet.sh. waiting for completion."
-
-wait
